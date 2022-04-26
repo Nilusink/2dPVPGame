52ae17a356059d9f318f1f8d3de21fc4700358c3
melektron <developer@elektron.work>
2022-04-26 18:40:46 +0200

    imporved / fixed gamesocket

--- core/gamesocket.py
+++ core/gamesocket.py
@@ -12,14 +12,14 @@ import json
 class GameSocket(socket.socket):
     
     input_buffer: bytes = b""
-    store_bytes: bool = False
-    msg_body: str = ""
 
     def send_packet(self, packet: dict) -> None:
         spacket = f"\x01{json.dumps(packet)}\x04"
         self.send(spacket.encode("ASCII"))
 
     def recv_packet(self) -> dict:
+        store_bytes: bool = False
+        msg_body: str = ""
         msg: bytes = b""
         while True:
             # include unused bytes from last receive
@@ -27,22 +27,20 @@ class GameSocket(socket.socket):
             for index, byte in enumerate(current_buffer):
                 match byte:
                     case 0x01:  # start marker
-                        self.store_bytes = True
-                        self.msg_body = ""
+                        store_bytes = True
 
                     case 0x04:  # end marker
                         if index == 0:
                             continue
 
-                        self.store_bytes = False
+                        store_bytes = False
                         # store rest of bytes for next call
                         self.input_buffer = current_buffer[index:]
                         # parse msg_body as json and return
                         return json.loads(self.msg_body)
 
                     case _:  # content
-                        if self.store_bytes:    # only store bytes between start and end marker
-                            self.msg_body += current_buffer[index:index+1].decode("ASCII")
-                        pass
-
+                        if store_bytes:    # only store bytes between start and end marker
+                            msg_body += current_buffer[index:index+1].decode("ASCII")
+            # read new bytes if end of message has not been reached
             msg: bytes = self.recv(1024)
